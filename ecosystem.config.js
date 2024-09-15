8de2b5ea3975d87c9e2d74e4287989909838b2cc
haonc <haodev.01@gmail.com>
2024-09-15 23:48:11 +0700

    feature -add code base

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,19 @@
 module.exports = {
   apps: [
     {
-      name: "NextBoilerplate",
-      exec_mode: "cluster",
-      instances: "max", // Or a number of instances
-      script: "node_modules/next/dist/bin/next",
-      args: "start",
+      name: 'NextBoilerplate',
+      exec_mode: 'cluster',
+      instances: 'max', // Or a number of instances
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start',
       env_local: {
-        APP_ENV: "local", // APP_ENV=local
+        APP_ENV: 'local', // APP_ENV=local
       },
       env_dev: {
-        APP_ENV: "dev", // APP_ENV=dev
+        APP_ENV: 'dev', // APP_ENV=dev
       },
       env_prod: {
-        APP_ENV: "prod", // APP_ENV=prod
+        APP_ENV: 'prod', // APP_ENV=prod
       },
     },
   ],
